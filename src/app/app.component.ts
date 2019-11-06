86e4f22356e26ef1bf6e8005c0ecfde6c851f5f9
草鞋没号 <308487730@qq.com>
2019-11-06 08:46:43 +0800

    简单的依赖注入 😁

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ReflectiveInjector, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-angular';
+
+  constructor() {
+    // ReflectiveInjector.resolveAndCreate 接收一个 perviders 数组，返回对象池 injector
+    const injector = ReflectiveInjector.resolveAndCreate([
+      // provide 为对象标识(token), useXxxx 是对象构造方式
+      { provide: Person, useClass: Person },
+      { provide: Id, useFactory: () => Id.getInstance('idcard') },
+      { provide: Address, useFactory: () => {
+        return new Address('浙江省', '杭州市', '西湖区', '灵隐街道');
+      } },
+    ]);
+    const person = injector.get(Person);
+    console.log(person);
+  }
+}
+
+class Id {
+  static type: string;
+  static getInstance(type: string): Id {
+    type = type;
+    return new Id();
+  }
+
+}
+
+class Address {
+  province: string;
+  city: string;
+  district: string;
+  street: string;
+
+  constructor(province: string, city: string, district: string, street: string) {
+    this.province = province;
+    this.city = city;
+    this.district = district;
+    this.street = street;
+  }
+}
+
+class Person {
+  id: Id;
+  address: Address;
+
+  constructor(
+    // @Inject(token) 注入依赖
+    @Inject(Id) id: Id,
+    @Inject(Address) address: Address
+  ) {
+    this.id = id;
+    this.address = address;
+
+    console.log(this.id, this.address);
+  }
 }
